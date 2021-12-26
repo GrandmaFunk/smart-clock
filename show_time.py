5246aec8dbe5b246976304d92b79afd50b900bda
Gino Crecco <gcrecco@gmail.com>
2021-12-26 01:41:41 -0500

    Clear display between time changes

--- show_time.py
+++ show_time.py
@@ -107,11 +107,12 @@ def update_memo(today):
     # Read the CSV file
     df = pd.read_csv('memos.csv')
     df['date'] = pd.to_datetime(df.date, format='%m/%d/%Y')
+    df.set_index(df.date, inplace=True)
     df['memo'] = df.memo.ffill()
 
     # Get today's memo
     #TODO: Error handle this
-    memo = df[df.date == today.strftime('%Y-%m-%d')].memo[0]
+    memo = df.loc[today.strftime('%Y-%m-%d')].memo
 
     try:
         last_memo = pickle.load(open('logs/last_memo.p', 'rb'))
@@ -120,6 +121,7 @@ def update_memo(today):
     
     if last_memo != memo:
         #TODO: Write the memo to Firebase
+        print(memo)
         pass
 
 def on_blink(doc, changes, read_time):
@@ -155,10 +157,27 @@ def show_blink():
             clock.show()
             time.sleep(.5)
 
-def show_clock():
+def set_time(time_to_set, brightness):
+    for pos, val in enumerate(time_to_set):
+        if pos == 0:
+            offset = 0
+        elif pos == 1:
+            offset = 4
+        elif pos == 2:
+            offset = 10
+        elif pos == 3:
+            offset = 14
+        for pixel in numbers[val]:
+            x = pixel[0] + offset
+            y = pixel[1]
+            clock.set_pixel(x,y, brightness,0,0)
+
+
+def refresh_clock():
     middle_on = True
     est = pytz.timezone('US/Eastern')
-    old_date = est.localize(datetime(2021,1,1))
+    old_date = datetime(2000, 1, 1)
+    old_time = '0000'
 
     while True:
         
@@ -167,35 +186,24 @@ def show_clock():
         current_time = datetime.now().strftime('%I%M')
 
         # Check if the date has changed
-        if (current_date - old_date).days > 0:
+        if current_date.date() != old_date.date():
             print('New day...')
             update_memo(current_date)
 
-        # Write the time
-        #TODO: Clear the previous number when a new time occurs
         #TODO: Remove the leading 0 for times 1 through 9
 
-        for pos, val in enumerate(current_time):
-            if pos == 0:
-                offset = 0
-            elif pos == 1:
-                offset = 4
-            elif pos == 2:
-                offset = 10
-            elif pos == 3:
-                offset = 14
-            for pixel in numbers[val]:
-                x = pixel[0] + offset
-                y = pixel[1]
-                clock.set_pixel(x,y, 200,0,0)
-
+        # Check if new time
+        if current_time != old_time:
+            set_time(old_time, 0)
+            set_time(current_time, 200)
         time.sleep(.5)
         old_date = current_date
+        old_time = current_time
 
 #TODO: Handle buttons
 #TODO: Send messages
 
-clock_thread = threading.Thread(target = show_clock)
+clock_thread = threading.Thread(target = refresh_clock)
 
 callback_done = threading.Event()
 blink_ref = db.collection(u'memos').document(u'blink')
